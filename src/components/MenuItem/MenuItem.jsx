cd7514daa5166b4ac5e00feb05ff2dc4afc41342
Christheo Guipo <christheo.guipo@gmail.com>
2025-07-04 22:36:32 +0200

    display heart icon and add styling for the wishlist button.

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -1,13 +1,20 @@
 import { useNavigate } from "react-router-dom";
 import Button from "../Button/Button";
 import styles from "./MenuItem.module.css";
+import { faHeart as faSolidHeart } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const MenuItem = ({ dish }) => {
+const MenuItem = ({ dish, isOnWishlist, handleToggleWishlist }) => {
   const { strMeal: name, strMealThumb: image } = dish;
   const navigate = useNavigate();
+
   return (
     <div className={styles.menuItem}>
       <h3>{name}</h3>
+      <FontAwesomeIcon className={styles.wishlistIcon}
+        onClick={() => handleToggleWishlist(dish)}
+        icon={isOnWishlist ? faSolidHeart : faHeart} />
       <img src={image} alt={name} />
       <div className={styles.menuItemBtnContainer}>
         <Button onClick={() => navigate(`/meals/${dish.idMeal}`)}>
